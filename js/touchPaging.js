e6398921ea3779625b6522d792db3faa71d8cca1
王军 <wangjun@JUNdeMacBook-Pro.local>
2015-03-28 11:13:09 +0800

    toggle succss page

--- js/touchPaging.js
+++ js/touchPaging.js
@@ -106,24 +106,31 @@
 
 
     // 分享默认配置
-    var defaults = {
+    var opts = {
         // 页面当前数
         pageNow       : 0,
         // 下一个index数
-        pageNext      : 0,
+        pageNext      : null,
         // 触摸开始获取的Y
         touchStartY   : 0,
+        // 触摸移动开始
+        moveStart     : true,
         // 触摸移动的方向
         movePosition  : null,
+        // 滑动的距离
+        touchDeltaY   : 0,
         // 移动第一次
         moveFirst     : true
     };
 
-
     function TouchPaging($this, options) {
 
         this.$page = $('.m-page');
-        this.options = $.extend(true, {}, defaults, options || {});
+        opts = $.extend(true, {}, opts, options || {});
+
+        opts.pageNumber = this.$page.length;
+
+        console.log( this.$page.size() );
 
         this.init();
     }
@@ -139,9 +146,17 @@
      * 事件开始
      */
     TouchPaging.prototype.onStart = function() {
-        this.$page.on('touchstart mousedown', this.touchStart);
-        this.$page.on('touchmove mousemove', this.touchMove);
-        this.$page.on('touchend mouseup', this.touchEnd);
+        var that = this;
+
+        that.$page.on('touchstart mousedown', function(event) {
+            that.touchStart(event);
+        });
+        that.$page.on('touchmove mousemove', function(event) {
+            that.touchMove(event);
+        });
+        that.$page.on('touchend mouseup', function(event) {
+            that.touchEnd(event);
+        });
     };
 
     /**
@@ -158,13 +173,19 @@
      */
     TouchPaging.prototype.touchStart = function(event) {
         var that = this;
+        var touchY = 0;
 
-        if ( event.type == "touchstart" ) {
-            this.options.touchStartY = event.touches[0].pageY;
-        } else {
-            this.options.touchStartY = event.pageY || event.y;
+        if ( !opts.moveStart ) {
+            return;
         }
 
+        if ( event.type == "touchstart" ) {
+            touchY = event.touches[0].pageY;
+        } else {
+            touchY = event.pageY || event.y;
+        }
+
+        opts.touchStartY = touchY;
     };
 
     /**
@@ -173,57 +194,81 @@
     TouchPaging.prototype.touchMove = function(event) {
         var moveY;
 
+        if ( !opts.moveStart ) {
+            return;
+        }
+
         if ( event.type == "touchmove" ) {
             moveY = event.touches[0].pageY;
         } else {
             moveY = event.pageY || event.y;
         }
 
-        var node = this.pagePosition();
+        var node = this.direction(event, moveY);
 
-            this.pageTranslate(node);
-
-    };
-
-    /**
-     * 触摸移动end
-     */
-    TouchPaging.prototype.touchEnd = function() {
-        var that = this;
-
-        
+        this.translate(node);
 
     };
 
     /**
-     * 触摸移动判断方向
+     * Move触摸移动判断方向
      */
-    TouchPaging.prototype.direction = function(event) {
-        var that = this;
+    TouchPaging.prototype.direction = function(event, moveY) {
+        var now, next, node,
+            $pageNow = this.$page.eq( opts.pageNow);
 
-        var now, next, node;
-
-        // if ( this.movePosition ) {
-
-        // }
-
-        this.pageNext += 1;
-
-        now = this.$page.eq( this.options.pageNow );
-        next = this.$page.eq( this.options.pageNext );
-        node = [next, now];
-
-        // move阶段根据方向设置页面的初始化位置--执行一次
-        if ( this.options.moveFirst ) {
-            initNext(node);
+        if ( moveY != 'undefined' ) {
+            opts.touchDeltaY = moveY - opts.touchStartY;
         }
 
-        function initNext(node) {
+        // 设置移动方向
+        if ( moveY - opts.touchStartY > 0 ) {
+            opts.movePosition = 'down';
+        } else {
+            opts.movePosition = 'up';
+        }
+
+        //设置下一页面的显示和位置
+        if ( opts.touchDeltaY <= 0 ) {
+            if ( $pageNow.next('.m-page').length == 0 ) {
+                opts.pageNext = 0;
+            } else {
+                opts.pageNext = opts.pageNow + 1;
+            }
+        } else {
+            if ( $pageNow.prev('.m-page').length == 0 ) {
+                return;
+            } else {
+                opts.pageNext = opts.pageNow - 1;
+            }
+        }
+
+        now = this.$page.eq( opts.pageNow )[0];
+        next = this.$page.eq( opts.pageNext )[0];
+        node = [now, next];
+
+        // move阶段根据方向设置页面的初始化位置--执行一次
+        if ( opts.moveFirst ) {
+            initNext(node, this.$page);
+        }
+
+        function initNext(node, $page) {
             var top, y,
                 _translateZ = Util.translateZ();
 
+            $page.removeClass('active');
+            $(node[0])
+                .removeClass('fn-hide');
+                //.addClass('active');
+
+            // 显示对应移动的page
+            $(node[1])
+                .removeClass('fn-hide')
+                .addClass('active');
+
+
             // 设置下一页的显示和位置
-            if ( that.options.movePosition == 'up' ) {
+            if ( opts.movePosition == 'up' ) {
                 top = parseInt( $(window).scrollTop() );
 
                 if ( top > 0 ) {
@@ -231,18 +276,14 @@
                 } else {
                     y = $(window).height();
                 }
-
-                node[0]
-                    .css({
-                        'transform': 'translate(0, ' + y + 'px)' + _translateZ
-                    })
-                    .attr('data-translate', y);
-                
-                
-                node[1].attr('data-translate', 0);
             } else {
-
+                y = -Math.max( $(window).height(), $(node[1]).height());
             }
+
+            node[1].style[Util.prefixStyle('transform')] = 'translate(0,' + y + 'px)' + _translateZ;
+
+            $(node[0]).attr('data-translate', 0);
+            $(node[1]).attr('data-translate', y);
 
         }
 
@@ -251,114 +292,163 @@
     };
 
     /**
-     * 触摸移动设置
+     * Move触摸移动设置
      */
     TouchPaging.prototype.translate = function(node) {
         var that = this;
 
         if ( !node ) return;
 
-        var _translateZ = Util.translateZ();
+        var _translateZ = Util.translateZ(),
+            touchDeltaY = opts.touchDeltaY,
+            scale = 1 - Math.abs( touchDeltaY * 0.2 / $(window).height()),
+            nextY, nowY;
+
+        //当前的页面移动
+        if ( $(node[0]).attr('data-translate') ) {
+            nowY = touchDeltaY + parseInt($(node[0]).attr('data-translate'));
+        }
+
+        node[0].style[Util.prefixStyle('transform')] = 'translate(0, ' + nowY + 'px)' + _translateZ + 'scale('+ scale +')';
+
+        //下一页移动
+        if ( $(node[1]).attr('data-translate') ) {
+            nextY = touchDeltaY + parseInt($(node[1]).attr('data-translate'));
+        }
+
+        node[1].style[Util.prefixStyle('transform')] = 'translate(0, ' + nextY + 'px)' + _translateZ;
+
+    };
+
+    /**
+     * 触摸移动end
+     */
+    TouchPaging.prototype.touchEnd = function() {
+
+        if ( !opts.moveStart ) {
+            return;
+        }
+
+        // 第一页UP阻止
+        if ( !opts.pageNext && opts.pageNext != 0 ) {
+            return;
+        }
         
+        opts.moveStart = false;
 
-        
+        var touchDeltaY = Math.abs( opts.touchDeltaY );
 
+        // 添加动画事件
+        if ( touchDeltaY > 10 ) {
+            this.$page.eq( opts.pageNow )[0].style[Util.prefixStyle('transition')] = 'all .3s';
+            this.$page.eq( opts.pageNext )[0].style[Util.prefixStyle('transition')] = 'all .3s';
+        }
 
+        // 切换页面，移动坐标大于100切换成功
+        if ( touchDeltaY >= 100 ) {
+            this.toggleSuccess();
+        } else {
+            this.toggleFail();
+        }
+
+        // 注销控制值
+        opts.movePosition = null;
+        opts.touchStartY = 0;
     };
 
     /**
      * 切换成功
      */
-    TouchPaging.prototype.pageSuccess = function() {
-        var that = this;
+    TouchPaging.prototype.toggleSuccess = function() {
+        var that = this,
+            _translateZ = Util.translateZ();
 
-        
+
+        // 当前的页面切换
+        var nowY = ( opts.touchDeltaY > 0 ) ? $(window).height() / 5 : -$(window).height() / 5,
+            scale = 0.1;
+
+        this.$page.eq( opts.pageNow )[0].style[Util.prefixStyle('transform')] = 'translate(0, '+ nowY +'px)' + _translateZ + 'scale('+ scale +')';
+
+        // 下一个页面的移动
+        this.$page.eq( opts.pageNext )[0].style[Util.prefixStyle('transform')] = 'translate(0, 0)' + _translateZ;
+
+        // 否为最后一页，显示或者隐藏箭头
+        if ( opts.next >= opts.pageNumber - 1 ) {
+            $('.fixed-arrow').addClass('fn-hide');
+        } else {
+            $('.fixed-arrow').removeClass('fn-hide');
+        }
+
+        // 删除添加的属性、Class
+        setTimeout(function() {
+
+            // 当前页面
+            that.$page.eq( opts.pageNow )
+                .addClass('fn-hide')
+                .removeClass('active')
+                .attr('data-translate', '')
+                .attr('style','');
+
+            that.$page.eq( opts.pageNext )
+                .addClass('active')
+                .attr('data-translate', '')
+                .attr('style','');
+
+            // 还原默认值
+            opts.touchDeltaY = 0;
+            opts.moveStart = true;
+            opts.pageNow = opts.pageNext;
+            opts.pageNext = null;
+
+        }, 300);
+
+
+    };
+
+    /**
+     * 切换失败
+     */
+    TouchPaging.prototype.toggleFail = function() {
+        var that = this,
+            _translateZ = Util.translateZ();
+
+        // 当前的页面
+        this.$page.eq( opts.pageNow )[0].style[Util.prefixStyle('transform')] = 'translate(0, 0)' + _translateZ + 'scale(1)';
+
+        var nextY;
+        if ( opts.movePosition == 'up' ) {
+            nextY = $(window).height();
+        } else {
+            nextY = -$(window).height();
+        }
+
+        this.$page.eq( opts.pageNext )[0].style[Util.prefixStyle('transform')] = 'translate(0, '+ nextY +'px)' + _translateZ;
+
+        // 删除添加的属性、Class
+        setTimeout(function() {
+
+            // 当前页面
+            that.$page.eq( opts.pageNow )
+                .addClass('active')
+                .attr('data-translate', '')
+                .attr('style','');
+
+            that.$page.eq( opts.pageNext )
+                .addClass('fn-hide')
+                .removeClass('active')
+                .attr('data-translate', '')
+                .attr('style','');
+
+            // 还原默认值
+            opts.touchDeltaY = 0;
+            opts.moveStart = true;
+            opts.pageNext = null;
+
+        }, 300);
     };
 
 
     new TouchPaging();
-
-    var Card = {
-        // 分页DOM
-        _page           : $('.m-page'),
-        // 页面当前数
-        _pageNow        : 0,
-        // 下一个index数
-        _pageNext       : 0,
-
-        // 触摸开始获取的Y
-        _touchStartY    : 0,
-        // 触摸移动的方向
-        _movePosition   : null,
-        // 移动第一次
-        _moveFirst      : true,
-        // 页面切换开始
-        pageStart: function() {
-
-            touch.on('body', 'swipeup', function(event) {
-                Card._movePosition = 'up';
-                Card.touchMove();
-            });
-
-            touch.on('body', 'swipedown', function(event) {
-                Card._movePosition = 'down';
-                Card.touchMove();
-            });
-        },
-        // 页面切换停止
-        pageStop: function() {
-            touch.off('body', 'swipeup', function(event) {
-            });
-
-            touch.off('body', 'swipedown', function(event) {
-            });
-        },
-        // 触摸移动Move
-        touchMove: function() {
-            var node = this.pagePosition();
-
-            this.pageTranslate(node);
-        },
-        // 触摸移动判断方向
-        pagePosition: function(event) {
-            var now, next, node;
-
-            if ( this._movePosition ) {
-
-            }
-
-            this._pageNext += 1;
-
-            now = this._page.eq( this._pageNow );
-            next = this._page.eq( this._pageNext );
-            node = [next,now];
-
-            // move阶段根据方向设置页面的初始化位置--执行一次
-            if ( this._moveFirst ) {
-                initNext(node);
-            }
-
-            function initNext(node) {
-                var _translateZ = Card._translateZ();
-
-            }
-
-
-            return node;
-        },
-        // 触摸移动设置
-        pageTranslate: function(node) {
-
-            console.log( node[0] );
-        },
-        init: function() {
-
-            // this.pageStart();
-            this.pageStart();
-
-        }
-    }
-
-    // Card.init();
 
 }));
